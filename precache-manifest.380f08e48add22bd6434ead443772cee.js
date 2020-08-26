c53eacdda7160d802003c6bde7c9cde73478535d
Leonidas51 <kirpichik61@gmail.com>
2020-08-26 13:49:09 +0300

    Updates

---
+++ precache-manifest.380f08e48add22bd6434ead443772cee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45af376083535476e9a9684d0d353b73",
+    "url": "/Mineswpr/index.html"
+  },
+  {
+    "revision": "0e61067c571706103e80",
+    "url": "/Mineswpr/static/css/main.7dc25038.chunk.css"
+  },
+  {
+    "revision": "b58aefcc215440d328ea",
+    "url": "/Mineswpr/static/js/2.73179935.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mineswpr/static/js/2.73179935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e61067c571706103e80",
+    "url": "/Mineswpr/static/js/main.781a7973.chunk.js"
+  },
+  {
+    "revision": "34a6456c94ea9b3fe2bc",
+    "url": "/Mineswpr/static/js/runtime-main.7a9281eb.js"
+  },
+  {
+    "revision": "291a09be1296fd18d07bd8a30c94369c",
+    "url": "/Mineswpr/static/media/victory.291a09be.jpg"
+  }
+]);
